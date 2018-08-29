9123382f0add560caf0f17f54cc9883586b69a2d
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-29 14:12:08 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(details)
+    @name = details[:name]
+    @weight = details[:weight]
+    @height = details[:height]
+    @@all << self
+  end
+
+  def self.clear
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def initialize(attr)
+    @name = attr[:name]
+    @type = attr[:type]
+    @booty = attr[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
